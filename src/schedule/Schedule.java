595547b5f1436c970d4f1e00b44f096f7437c300
20078 <20078@MXL52836QZ.columbushs.com>
2018-08-23 13:16:56 -0400

    initial commit

---
+++ src/schedule/Schedule.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+/**
+ *
+ * @author 20078
+ */
+public class Schedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String firstName = "Olivier";
+        String lastName = "Clement";
+        int gradYear = 2020;    
+         System.out.println(firstName + " " + lastName + " " + "is my name"+ " " + "and I graduate in" + " " + gradYear + ".");
+         System.out.println("+-----------------------------------------------+\n" +
+"|P1|    |Sports Management    |  Roger Bowman    |\n" +
+"|P2|    |AP Computer Science  |  Matthew larrubia|\n" +
+"|P3|    |Accelerated English  |  Andrew Chacon   |\n" +
+"|P4|    |Environmetal Science |  Chris Connell   |\n" +
+"|P5|    |Hon Global Studies   |  James Dugard    |\n" +
+"|P6|    |College Alg & Trig   |  Maria Acevedo   |\n" +
+"|P7|    |Hon American History |  John Cunningham |\n" +
+"+-----------------------------------------------+");
+    }
+   
+    
+}
